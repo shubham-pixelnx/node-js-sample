306c8eba77b52fedd8b21feae22dc1563ba614ae
Shubham Prajapat <shubham.prajapat@himanshusofttech.com>
2020-08-26 17:37:27 +0530

    Port 80

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let express = require("express");
 let app = express();
 
-app.set("port", 8080);
+app.set("port", 80);
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function (request, response) {
