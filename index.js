7ea0a11408bcb8e96d6b98e2ee020c6f002a3e93
Shubham Prajapat <shubham.prajapat@himanshusofttech.com>
2020-08-26 17:17:36 +0530

    EC2 commit

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var express = require('express')
-var app = express()
+let express = require("express");
+let app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set("port", 8080);
+app.use(express.static(__dirname + "/public"));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+app.get("/", function (request, response) {
+    response.send("Hello World!");
+});
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+app.listen(app.get("port"), function () {
+    console.log("Node app is running at localhost:" + app.get("port"));
+});
